306090257c39638a003b68498f35b5e9011482b6
llthanh714@live.com <llthanh714@live.com>
2022-11-15 21:16:45 +0700

    Change function follow IIFE

--- pages/pokemon.tsx
+++ pages/pokemon.tsx
@@ -2,40 +2,38 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import { useEffect, useState, useRef } from "react";
 
-interface Pokemon {
+interface IPokemon {
   name: string;
   url: string;
 }
 
-interface apiResponse {
+interface IApiResponse {
   count: number;
-  results: Pokemon[];
+  results: IPokemon[];
   next: string | null;
   previous: string | null;
 }
 
 function PokemonList() {
-  const [data, setData] = useState<apiResponse>();
+  const [data, setData] = useState<IApiResponse>();
   const [offset, setOffset] = useState(0);
   const limit = useRef(50);
 
-  const getPokemons = async () => {
-    await axios
-      .get("https://pokeapi.co/api/v2/pokemon", {
-        params: {
-          limit: limit.current,
-          offset: offset,
-        },
-      })
-      .then((response) => setData(response.data))
-      .catch((error) => {
-        console.log(error);
-        return [];
-      });
-  };
-
   useEffect(() => {
-    getPokemons()
+    (async function () {
+      await axios
+        .get("https://pokeapi.co/api/v2/pokemon", {
+          params: {
+            limit: limit.current,
+            offset: offset,
+          },
+        })
+        .then((response) => setData(response.data))
+        .catch((error) => {
+          console.log(error);
+          return [];
+        });
+    })();
   });
 
   return (
@@ -44,19 +42,24 @@ function PokemonList() {
       <nav aria-label="Page navigation example">
         <ul className="pagination justify-content-center">
           <li className="page-item">
-            <a id="pre-btn" className="page-link"
-            onClick={() => {
-                if (!!data?.previous)
-                setOffset((prev) => prev - limit.current);
-            }}>
+            <a
+              id="pre-btn"
+              className="page-link"
+              onClick={() => {
+                if (!!data?.previous) setOffset((prev) => prev - limit.current);
+              }}
+            >
               Previous
             </a>
           </li>
           <li className="page-item">
-            <a id="next-btn" className="page-link" onClick={() => {
-                if (!!data?.next)
-                setOffset((prev) => prev + limit.current);
-            }}>
+            <a
+              id="next-btn"
+              className="page-link"
+              onClick={() => {
+                if (!!data?.next) setOffset((prev) => prev + limit.current);
+              }}
+            >
               Next
             </a>
           </li>
@@ -71,7 +74,7 @@ function PokemonList() {
           </tr>
         </thead>
         <tbody>
-          {data?.results.map((pokemon: Pokemon, index: number) => (
+          {data?.results.map((pokemon: IPokemon, index: number) => (
             <tr key={index}>
               <td>{index + offset + 1}</td>
               <td>{pokemon.name}</td>
